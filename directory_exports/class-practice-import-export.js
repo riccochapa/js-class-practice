5301df5b2bf64857b68d2e4b834c1ee1e8e8a6d5
Ricco <riccochapa@gmail.com>
2016-07-01 13:55:27 -0500

    update

---
+++ directory_exports/class-practice-import-export.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _classPractice = require("./class-practice.js");
+
+var fee = "";
+var percentage = "";
+var withdraw = "";
+var sumOfBalance = 0;
+
+var acc1 = new _classPractice.BankAccount('Jimmy Jonas');
+var acc2 = new _classPractice.SavingsAccount('Debbie Boone');
+var text = new _classPractice.BankAccount("Tommy Sanss");
+
+acc1.withdrawal(222);
+text.withdrawal(333333);
